0c9daecf31165bd2beda8235533658fa5262864a
sharafdeen <35745895+sharafdeen@users.noreply.github.com>
2018-06-28 17:56:02 +0100

    File added through file upload
    Four files were added

---
+++ designs.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    var height, width;
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    makeGrid(height, width);
+    putColor()
+});
+
+// Your code goes here!
+function makeGrid(row, col) {
+    $('tr').remove();
+    for (var i = 1; i <= row; i++) {
+       // $('table').append('<tr></tr>');
+        $('#pixelCanvas').append('<tr></tr>');
+        for (var j = 1; j <=col; j++) {
+            //$('tr').append('<td></td>');
+           $('tr').filter(':last').append('<td></td>');
+        }
+    }
+}
+
+// add colors to the cells
+function putColor() {
+    var col = $('td');
+    col.click(function () {
+    var color = $('#colorPicker').val();
+    if ($(this).attr('style')) {
+        $(this).removeAttr('style');
+     } else {
+        $(this).attr('style', 'background-color:' + color);
+     }
+   });
+}
